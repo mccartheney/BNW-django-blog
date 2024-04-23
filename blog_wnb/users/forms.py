a0cca4dcb2750d5418e70732fc2a81b1dde7ad08
Mccartheney <mccartheney@gmail.com>
2024-04-23 10:37:19 +0100

    finalized users authentication

--- blog_wnb/users/forms.py
+++ blog_wnb/users/forms.py
@@ -2,7 +2,17 @@ from django import forms
 from .models import user_profile
 from django.forms import ModelForm
 
-class register (ModelForm) :
+class register_form (ModelForm) :
     class Meta :
         model = user_profile
         fields = ['email', 'username', 'password', 'bio', 'profile_pic']
+        widgets = {
+            'password': forms.PasswordInput()
+        }
+class login_form (ModelForm) :
+    class Meta :
+        model = user_profile
+        fields = ["email", "password"]
+        widgets = {
+            'password': forms.PasswordInput()
+        }
--- blog_wnb/users/views.py
+++ blog_wnb/users/views.py
@@ -1,58 +1,83 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login as auth_login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login as auth_login, logout, authenticate
 from django.contrib.auth.models import User
 
-from .forms import register
+from .forms import register_form, login_form
 from .models import user_profile
 
 def user_home(request):
     return redirect('users:login')
 
 def register_view(request):
-    # if request.method == 'POST':
-    #     form = UserCreationForm(request.POST)
-    #     if form.is_valid():
-    #         user_data = form.save()
-    #         auth_login(request, user_data)
-    #         return redirect('/')
-    # else:
-    #     form = UserCreationForm()
-    
-    if request.method == "POST" :
-        form = register(request.POST, request.FILES)
-        if form.is_valid () :
-            email_from_form = form.cleaned_data["email"]
-            user_profile_exists = user_profile.objects.filter(email=email_from_form).exists()
-            if user_profile_exists :
-                warn_message = " ⚠️ This email is alredy been used, try another "
-                return render(request, "users/register.html", {'form': form, "message": warn_message})
+    is_logged = request.user.is_authenticated
+    if not is_logged :
 
-            else :
-                form_data = form.cleaned_data
+        if request.method == "POST" :
+            form = register_form(request.POST, request.FILES)
+            if form.is_valid () :
+                email_from_form = form.cleaned_data["email"]
+                user_profile_exists = user_profile.objects.filter(email=email_from_form).exists()
+                if user_profile_exists :
+                    warn_message = " ⚠️ This email is alredy been used, try another "
+                    return render(request, "users/register.html", {'form': form, "message": warn_message})
 
-                User.objects.update_or_create (
-                    email = form_data ["email"],
-                    username = form_data ["username"],
-                    password = form_data ["password"]
-                )
+                else :
+                    form_data = form.cleaned_data
 
-                form.save()
-                return redirect("/")
+                    new_user = User.objects.create_user (
+                        email = form_data ["email"],
+                        username = form_data ["username"],
+                        password=form_data["password"],
+                    )
 
+                    form.save()
+                    return redirect("/")
+
+        else :
+            form = register_form()
+        return render(request, "users/register.html", {'form': form})
     else :
-        form = register()
-    return render(request, "users/register.html", {'form': form})
+        return redirect("/")
+
 
 def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            auth_login(request, form.get_user())
-            return redirect('/')  
-    else:
-        form = AuthenticationForm()
-    return render(request, "users/login.html", {'form': form})
+    is_logged = request.user.is_authenticated
+    if not is_logged :
+
+        if request.method == 'POST':
+            form = login_form(request.POST)
+            if form.is_valid():
+                form_data = form.cleaned_data
+                user_get_profile = user_profile.objects.filter(email = form_data["email"])
+                user_exists = user_get_profile.exists()
+
+
+                if user_exists :
+                    user_profile_data = user_get_profile[0]
+
+                    username_data = user_profile_data.username
+                    password_data = form_data["password"]
+
+                    user = authenticate(request, username=username_data, password=password_data)
+                    if user is not None:
+                        # Authentication succeeded
+                        auth_login(request, user)
+                        return redirect('/')
+
+                    else :
+                        warn_message = "⚠️ Wrong credencials" 
+                        return render(request, "users/login.html", {'form': form, "message" : warn_message})
+
+                else :
+                    warn_message = " ⚠️ Dont exist a account with that email, login with a existent email or register "
+                    return render(request, "users/login.html", {'form': form, "message" : warn_message})
+
+        else:
+            form = login_form()
+        return render(request, "users/login.html", {'form': form})
+    else :
+        return redirect("/")
+        
 
 def logout_view(request):
     logout(request)
